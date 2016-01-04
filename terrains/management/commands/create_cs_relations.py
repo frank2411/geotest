799100d31f75208a584fc44883a36e89998449ad
frank2411 <francesco.perna90@gmail.com>
2016-01-04 04:13:31 +0100

    added tests and improved command for associations

--- terrains/management/commands/create_cs_relations.py
+++ terrains/management/commands/create_cs_relations.py
@@ -6,13 +6,81 @@ from django.db.models import Avg, Max, Min
 class Command(BaseCommand):
     help = 'Create relations between the terrains and the circonscriptions'
 
+    # MONTREAL_CIRCONSCRIPTIONS = [
+    #     u"Ahuntsic-Cartierville",
+    #     u"Bourassa",
+    #     u"Hochelaga",
+    #     u"Honor\u00e9-Mercier",
+    #     u"LaSalle--\u00c9mard--Verdun",
+    #     u"Lac-Saint-Louis",
+    #     u"La Pointe-de-l'\u00cele",
+    #     u"Dorval--Lachine--LaSalle",
+    #     u"Laurier--Sainte-Marie",
+    #     u"Mont-Royal",
+    #     u"Notre-Dame-de-Gr\u00e2ce--Westmount",
+    #     u"Outremont",
+    #     u"Papineau",
+    #     u"Pierrefonds--Dollard",
+    #     u"Rosemont--La Petite-Patrie",
+    #     u"Saint-Laurent",
+    #     u"Saint-L\u00e9onard--Saint-Michel",
+    #     u"Ville-Marie--Le Sud-Ouest--\u00cele-des-Soeurs",
+    # ]
+
+    # List of fednum for montreal circonscriptions
+    MONTREAL_CIRCONSCRIPTIONS = [
+        24003,
+        # u"Ahuntsic-Cartierville",
+        24015,
+        # u"Bourassa",
+        24028,
+        # u"Hochelaga",
+        24029,
+        # u"Honor\u00e9-Mercier",
+        24037,
+        # u"LaSalle--\u00c9mard--Verdun",
+        24036,
+        # u"Lac-Saint-Louis",
+        24033,
+        # u"La Pointe-de-l'\u00cele",
+        24024,
+        # u"Dorval--Lachine--LaSalle",
+        24039,
+        # u"Laurier--Sainte-Marie",
+        24052,
+        # u"Mont-Royal",
+        24053,
+        # u"Notre-Dame-de-Gr\u00e2ce--Westmount",
+        24054,
+        # u"Outremont",
+        24055,
+        # u"Papineau",
+        24056,
+        # u"Pierrefonds--Dollard",
+        24064,
+        # u"Rosemont--La Petite-Patrie",
+        24068,
+        # u"Saint-Laurent"
+        24069,
+        # u"Saint-L\u00e9onard--Saint-Michel"
+        24077,
+        # u"Ville-Marie--Le Sud-Ouest--\u00cele-des-Soeurs"
+    ]
+
     def handle(self, *args, **options):
         terrains = Terrain.objects.all()
 
         for terrain in terrains:
-            circonscription = Circonscription.objects.get(
-                geom__contains=terrain.coordinates, provcode="QC")
+            try:
+                circonscription = Circonscription.objects.get(
+                    geom__contains=terrain.coordinates,
+                    fednum__in=self.MONTREAL_CIRCONSCRIPTIONS)
+            except Circonscription.DoesNotExist:
+                print "TERRAIN IS OUT OF MONTREAL"
+                continue
+
             circonscription.terrains.add(terrain)
+            print "TERRAIN ASSOCIATED"
 
         circonscriptions = Circonscription.objects.filter(
             terrains__isnull=False).distinct().annotate(
--- terrains/tests.py
+++ terrains/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from terrains.models import Terrain
+import os
+from django.core.files.base import ContentFile
 
-# Create your tests here.
+
+class TerrainsTestCase(TestCase):
+
+    def setUp(self):
+        base_path = os.path.dirname(__file__)
+        test_csv_dirname = "test_csv"
+        test_csv_filename = "test.csv"
+        self.csvfilepath = os.path.join(
+            base_path, test_csv_dirname, test_csv_filename)
+        csvfile = ContentFile(open(self.csvfilepath))
+        # I simulate the filefield path attribute
+        # only for test purposes
+        csvfile.path = self.csvfilepath
+        Terrain.generate_terrain_from_csv(csvfile)
+
+    def test_terrains_count(self):
+        self.assertEquals(Terrain.objects.all().count(), 14)
+
+    def test_ranges_length(self):
+        ranges = Terrain.generate_ranges(Terrain.objects.all(), None)
+        self.assertEquals(len(ranges), 8)
