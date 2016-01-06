d5c54287f91341ef6640d883ead77f177b56c6f8
frank2411 <francesco.perna90@gmail.com>
2016-01-06 15:14:05 +0100

    cleaned custom utils files

--- custom_utils/urls.py
+++ custom_utils/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from custom_utils.views import *
 
-urlpatterns = (
-    url(r'^', home, name='home'),
-)
+# urlpatterns = (
+#     url(r'^', home, name='home'),
+# )
